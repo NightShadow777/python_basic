267f51800cf13e15df485ef5fb5697511c5b8ebf
Night <rudolf_miller@ukr.net>
2022-11-11 13:29:59 +0200

    другой вариант, проверка

--- L3/dz6.py
+++ L3/dz6.py
@@ -1,9 +1,9 @@
 #my_list = [12, 3, 4, 10]
 #my_list = [1]
 #my_list = []
-#my_list = [6, 5, 3, 1]
+my_list = [6, 5, 3, 1]
 #my_list = [6, 5, 3, 1, 10, 40, 35]
-my_list = [6, 5, 3, 1, 10, 40, 35, 100, 50, 785, 908]
+#my_list = [6, 5, 3, 1, 10, 40, 35, 100, 50, 785, 908]
 
 if len(my_list) > 1:
     get_el = my_list.pop()
@@ -11,7 +11,7 @@ if len(my_list) > 1:
     print(my_list)
 else:
     print(my_list)
-'''
+
 print("####" * 10)
 
 #other
@@ -22,4 +22,3 @@ if len(my_list) > 1:
     print(my_list)
 else:
     print(my_list)
-'''
