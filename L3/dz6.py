7b0c84afe99f4034614f72f1616c2c4d319e137e
Night <rudolf_miller@ukr.net>
2022-11-11 13:23:04 +0200

    fix add new list line 6 - dz6

--- L3/dz6.py
+++ L3/dz6.py
@@ -1,8 +1,9 @@
 #my_list = [12, 3, 4, 10]
 #my_list = [1]
 #my_list = []
-my_list = [6, 5, 3, 1]
-my_list = [6, 5, 3, 1, 10, 40, 35]
+#my_list = [6, 5, 3, 1]
+#my_list = [6, 5, 3, 1, 10, 40, 35]
+my_list = [6, 5, 3, 1, 10, 40, 35, 100, 50, 785, 908]
 
 if len(my_list) > 1:
     get_el = my_list.pop()
