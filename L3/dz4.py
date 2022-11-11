40745ae402762e253fd64c7717bb3d81997b9bbd
Night <rudolf_miller@ukr.net>
2022-11-11 15:29:23 +0200

    send to git simple calc

--- L3/dz4.py
+++ L3/dz4.py
@@ -2,21 +2,23 @@ number_1 = float(input("Введите число 1: "))
 number_2 = float(input("Введите число 2: "))
 type_operation = input("Выберите операцию (+ , - , / , *): ")
 
+number_1 = int(number_1)
+number_2 = int(number_2)
+
 if type_operation == "+":
-    result = int(number_1) + int(number_2)
-    print(result)
+    result = number_1 + number_2
+    print(int(result))
 elif type_operation == "-":
-    result = int(number_1) - int(number_2)
-    print(result)
+    result = number_1 - number_2
+    print(int(result))
 elif type_operation == "/":
     if number_2:
-        result = int(number_1) / int(number_2)
-        print("Делитель не равен 0")
-        print(result)
+        result = number_1 / number_2
+        print(int(result))
     else:
-        print("Делитель равен 0")
+        print("Деление на ноль не определено")
 elif type_operation == "*":
-    result = int(number_1) * int(number_2)
-    print(result)
+    result = number_1 * number_2
+    print(int(result))
 else:
     print("Ничего не выбрано")
