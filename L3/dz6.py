35ad7685573e764187bd8c441bc131e469c5172f
Night <rudolf_miller@ukr.net>
2022-11-11 10:47:01 +0200

    add dz6

---
+++ L3/dz6.py
@@ -0,0 +1,11 @@
+my_list = [12, 3, 4, 10]
+#my_list = [1]
+#my_list = []
+#my_list = [6, 5, 3, 1]
+
+if len(my_list) > 1:
+    get_el = my_list.pop(3)
+    my_list.insert(0, get_el)
+    print(my_list)
+else:
+    print(my_list)
