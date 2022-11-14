0be77cc84cb8239bc106f1388acb47c49c1c021d
Night <rudolf_miller@ukr.net>
2022-11-14 09:39:17 +0200

    add code to file dz5

--- L3/dz5.py
+++ L3/dz5.py
@@ -1,8 +1,26 @@
-my_list = [1, 2, 3, 4, 5, 6]
-#my_list = [1, 2, 3]
-#my_list = [1, 2, 3, 4, 5]
-#my_list = [1]
-#my_list = []
-
+'''
+[1, 2, 3, 4, 5, 6] => [[1, 2, 3], [4, 5, 6]]
+[1, 2, 3] => [[1, 2], [3]]
+[1, 2, 3, 4, 5] => [[1, 2, 3], [4, 5]]
+[1] => [[1], []]
+[] => [[], []]
+'''
+a = [1,2,3,4,5,6]
+#a = [1,2,3,4,5]
+#a = [1,2,3]
+#a = [1]
+#a = []
 new_list = []
-
+number = len(a)
+if (number % 2) == 0:
+	#print("четное")
+	indx = number // 2
+	new_list.append(a[:indx])
+	new_list.append(a[indx:])
+	print(new_list)
+else:
+	#print("нечетное")
+	indx = (number // 2)+1
+	new_list.append(a[:indx])
+	new_list.append(a[indx:])
+	print(new_list)
