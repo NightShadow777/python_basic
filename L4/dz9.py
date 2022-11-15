cae6a8e67d820dc970afb762db32aca2dca7a139
Night <rudolf_miller@ukr.net>
2022-11-15 13:31:03 +0200

    brought some beauty

--- L4/dz9.py
+++ L4/dz9.py
@@ -1,9 +1,10 @@
-import random
-new_list = []
-orign_list = [l for l in range(random.randint(3, 10))]
 #orign_list = [1, 2, 3, 4, 5, 6, 7, 9]
 #orign_list = [1, 1, 1, 1]
 #orign_list = [6, 3, 7]
+
+import random
+new_list = []
+orign_list = [l for l in range(random.randint(3, 10))]
 print("Origin list: ", end="")
 print(orign_list)
 new_list.append(orign_list[0])
