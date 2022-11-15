255df8d33326594cea48f19d7a45c3c170a4b314
Night <rudolf_miller@ukr.net>
2022-11-15 13:25:11 +0200

    мелкие правки dz9.py

--- L4/dz9.py
+++ L4/dz9.py
@@ -4,10 +4,10 @@ orign_list = [random.randint(3, 10) for l in range(random.randint(3, 10))]
 #orign_list = [1, 2, 3, 4, 5, 6, 7, 9]
 #orign_list = [1, 1, 1, 1]
 #orign_list = [6, 3, 7]
-print("First list: ", end="")
+print("Origin list: ", end="")
 print(orign_list)
 new_list.append(orign_list[0])
 new_list.append(orign_list[2])
 new_list.append(orign_list[-2])
-print("Second list: ", end="")
+print("New list: ", end="")
 print(new_list)
