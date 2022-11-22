7c3b2a23a8e3a5204310b79d4cddf8c53561767f
Night <rudolf_miller@ukr.net>
2022-11-22 13:45:30 +0200

    add dz12

---
+++ L5/dz12.py
@@ -0,0 +1,20 @@
+import string
+#Python Community
+data = str(input("Type string: "))
+res_list = []
+for symbol in string.punctuation:
+    if symbol in data:
+        data = data.replace(symbol, " ")
+
+data = data.split()
+for word in data:
+    res_list.extend(word.strip().title())
+    result = "".join(res_list)
+
+if len(result) > 140:
+    new_result = result[:140-1]
+    print("#{}".format(new_result))
+else:
+    print("#{}".format(result))
+
+
