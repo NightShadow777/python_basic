946643abc3bd0443e2458ca51a52d3c75c4a49f4
Night <rudolf_miller@ukr.net>
2022-11-23 17:49:59 +0200

    dz10 нужно переделать

--- L5/dz10.py
+++ L5/dz10.py
@@ -1,17 +1,38 @@
 import string
 import keyword
-'''
-elif data.isupper() or data in string.punctuation:
-    print("false")
-elif "_" in data:
-    print("true")
-elif data in keyword.kwlist:
-    print("false")
-'''
+data = input("Type string: ")
 
+if data:
+    symbol =[]
+    for s in string.punctuation:
+        if s == '_':
+            del_symbol = s.replace("_", "")
+        else:
+            del_symbol = s
+        symbol.extend(del_symbol)
 
-data = str(input("Type string: "))
+    w = []
+    for word in data:
+        w.extend(word)
 
-for char in data:
-    if char.isdigit():
-        print("false")
+    #не может начинаться с цифры
+    #не может состоять только из цифр
+    #не может содержать заглавные буквы
+    #не может содержать знаки пунктуации
+    #не может содержать зарегистрированные слова keyword.kwlist
+    for char in w:
+        if data[:1].isdigit():
+            print(False)
+        elif data.isdigit():
+            print(False)
+        elif char.isalpha() and char.isupper():
+            print(False)
+        elif char in symbol:
+            print(False)
+        elif w in keyword.kwlist:
+            print(False)
+        else:
+            print(True)
+
+else:
+    print("Введите переменную для проверки")
