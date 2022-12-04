111d48ab015c696e4dae212f3fac20e1308b16fe
rudolf_miller@ukr.net <rudolf_miller@ukr.net>
2022-12-04 17:25:34 +0200

    del task text

--- L6/dz13.py
+++ L6/dz13.py
@@ -1,23 +1,14 @@
-'''
-Пользователь вводит через дефис две буквы, Ваша задача написать программу, которая будет возвращать все символы между ними включительно.
-Никаких проверок на ошибку делать не надо, минимальное значение всегда
-меньше или равно максимальному.
-Подсказка: Используйте модуль string , в котором есть string.ascii_letters, со всем набором необходимых букв
-Пример:
-"a-c" -> abc
-"a-a" -> a
-"s-H" -> stuvwxyzABCDEFGH
-"a-A" -> abcdefghijklmnopqrstuvwxyzA
-'''
-
 import string
 data = input("Ввод: ")
+
 clean_data = data.replace("-", "")
 start = min(clean_data)
 end = max(clean_data)
+
 letters = string.ascii_letters
 s = letters.find(start)
 e = letters.find(end)
+
 res = letters[s:e+1]
 if not res:
     res = letters[e:s + 1]
