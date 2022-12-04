608393ad7c5c922dccdfd79082cb13f1c4ba4920
rudolf_miller@ukr.net <rudolf_miller@ukr.net>
2022-12-04 18:06:28 +0200

    add code dz14

--- L6/dz14.py
+++ L6/dz14.py
@@ -7,3 +7,17 @@
 423 -> 8
 1 -> 1
 '''
+
+data = input("Ввод: ")
+if int(data) > 0:
+    s = 1
+    while True:
+        s = 1
+        for n in data:
+            s *= int(n)
+        data = str(s)
+        print(data)
+        if int(data) <= 9:
+            break
+else:
+    print("Stop!")
