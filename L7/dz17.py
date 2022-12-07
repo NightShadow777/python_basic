486025da78f80c42bb4afe7b982378020ed5c05a
Night <rudolf_miller@ukr.net>
2022-12-07 12:20:15 +0200

    добавил проверку на пустоту param

--- L7/dz17.py
+++ L7/dz17.py
@@ -1,20 +1,23 @@
 def correct_sentence(param):
-    get_first_letter = param[:1]
-    word_list = []
-    i = 0
-    for w in param:
-        if i == 0:
-            w = w.replace(get_first_letter, w.upper())
-            word_list.extend(w)
+    if param:
+        get_first_letter = param[:1]
+        word_list = []
+        i = 0
+        for w in param:
+            if i == 0:
+                w = w.replace(get_first_letter, w.upper())
+                word_list.extend(w)
+            else:
+                word_list.extend(w)
+            i += 1
+        param = "".join(word_list)
+
+        if "." in param:
+            print(param)
         else:
-            word_list.extend(w)
-        i += 1
-    param = "".join(word_list)
-
-    if "." in param:
-        print(param)
+            print(param + ".")
     else:
-        print(param + ".")
+        print("Введите строку!!!")
 
-data  = input("Введите строку: ")
+data  = input("Ввод: ")
 correct_sentence(data)
