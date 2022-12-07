7a6670402dc6ab4709d7bee8dec4e1dcfc606d38
Night <rudolf_miller@ukr.net>
2022-12-07 12:17:32 +0200

    add 2 dz17

--- L7/dz17.py
+++ L7/dz17.py
@@ -1,15 +1,15 @@
 def correct_sentence(param):
     get_first_letter = param[:1]
-    if get_first_letter.islower():
-        word_list = []
-        for w in param:
-            if get_first_letter == w:
-                w = w.replace(get_first_letter, w.upper())
-                word_list.extend(w)
-            else:
-                word_list.extend(w)
-
-        param = "".join(word_list)
+    word_list = []
+    i = 0
+    for w in param:
+        if i == 0:
+            w = w.replace(get_first_letter, w.upper())
+            word_list.extend(w)
+        else:
+            word_list.extend(w)
+        i += 1
+    param = "".join(word_list)
 
     if "." in param:
         print(param)
