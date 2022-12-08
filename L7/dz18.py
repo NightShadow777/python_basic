7ee800760c3843d30db0fbc0920d01d3a48087d4
Night <rudolf_miller@ukr.net>
2022-12-08 12:02:46 +0200

    dz18 ...

--- L7/dz18.py
+++ L7/dz18.py
@@ -1,14 +1,9 @@
 def second_index(istring, wanted_symbol):
-    if istring and wanted_symbol:
-        result = None
-        for i, s in enumerate(istring):
-            if i > 0 and wanted_symbol == s:
-                result = i
-    else:
-        result = "Error!"
-    return result
+    string_index = istring.find(wanted_symbol, istring.find(wanted_symbol) + 1)
+    if string_index == -1:
+        string_index = None
+    print(string_index)
 
 istring = input("Введите строку: ")
 wanted_symbol = input("Символ для поиска: ")
-s_index = second_index(istring, wanted_symbol)
-print(s_index)
+second_index(istring, wanted_symbol)
