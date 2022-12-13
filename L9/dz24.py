af277be2503bdb04d60bf32238f9194ac9361e94
Nightshadow777 <rudolf_miller@ukr.net>
2022-12-14 00:10:08 +0200

    dz24 add code

--- L9/dz24.py
+++ L9/dz24.py
@@ -14,5 +14,23 @@ difference (10.2, -2.2, 0, 1.1, 0.5) == 12.4
 difference () == 0
 '''
 
-def difference():
-    pass
+def difference(*args):
+    if args:
+        min_element = args[0]
+        max_element = args[-1]
+        for element in args:
+            if element < min_element:
+                min_element = element
+
+            if element > max_element:
+                max_element = element
+        result = max_element - min_element
+        return result
+    else:
+        result = 0
+        return result
+
+print(difference (1, 2, 3))
+print(difference (5, -5))
+print(difference (10.2, -2.2, 0, 1.1, 0.5))
+print(difference ())
