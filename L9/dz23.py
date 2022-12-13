1ae300b7efbd9225dd716f4acd6c98c239f5ffe3
Nightshadow777 <rudolf_miller@ukr.net>
2022-12-13 23:09:31 +0200

    add dz 23

--- L9/dz23.py
+++ L9/dz23.py
@@ -22,6 +22,7 @@ def popular_words(text, keywords):
     for text in text_list:
         for keyword in keywords:
             if keyword == text.lower():
+                #print(text_list)
                 n = text_list.count(text)
                 res_dict[keyword] = n
             else:
@@ -29,6 +30,5 @@ def popular_words(text, keywords):
                     res_dict[keyword] = 0
     return res_dict
 
-popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near'])
-popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near'])
-popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near'])
+print(popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']))
+print(popular_words('''Interesting Facts For Curious Minds gives you the answer to all these and many many more questions that I know have crossed your mind from time to time . This book is divided into 63 chapters by topic for your convenience, bringing you a nice mix of science, history, pop culture, and all sorts of stuff in between. Each chapter contains 25 concise yet engaging factoids that are sure to make you think and at times laugh. ''', ['many', 'time', 'book', 'read']))
