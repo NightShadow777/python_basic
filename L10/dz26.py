0c0b75114d0d108c19dd1397decbd193f560ea6f
Nightshadow777 <rudolf_miller@ukr.net>
2022-12-16 00:05:56 +0200

    добавил дз26

---
+++ L10/dz26.py
@@ -0,0 +1,12 @@
+def first_word(i_string):
+    i_string = i_string.replace(".", " ").replace(",", " ")
+    new_string = i_string.strip().split()
+    return new_string[0]
+
+print(first_word("Hello world"))
+print(first_word("greetings, friends"))
+print(first_word("don't touch it"))
+print(first_word("... and so on ..."))
+print(first_word("hi"))
+print(first_word("Hello.World"))
+
