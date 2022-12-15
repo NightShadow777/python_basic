14fff3c003dc2f65ce33cea02a135ce97d4ca107
Nightshadow777 <rudolf_miller@ukr.net>
2022-12-15 23:23:19 +0200

    добавил проверку для нецелого числа dz27

--- L10/dz27.py
+++ L10/dz27.py
@@ -1,9 +1,14 @@
 def is_even(param):
-    if (param % 2) == 0:
-        return True
+    if int(param) == param:
+        if (param % 2) == 0:
+            return True
+        else:
+            return False
     else:
-        return False
+        return "Число нецелое..."
 
 print(is_even(2))
 print(is_even(5))
 print(is_even(0))
+print(is_even(2.1))
+print(is_even(2.3))
