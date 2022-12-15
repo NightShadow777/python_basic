686e53dde5a22ff4b4611a652c2094c3e5e11beb
Nightshadow777 <rudolf_miller@ukr.net>
2022-12-15 23:11:29 +0200

    add dz27

---
+++ L10/dz27.py
@@ -0,0 +1,12 @@
+def is_even(param):
+    if (param % 2) == 0:
+        return True
+    else:
+        return False
+
+print(is_even(2))
+print(is_even(5))
+print(is_even(0))
+print(is_even(4))
+print(is_even(3))
+print(is_even(10))
