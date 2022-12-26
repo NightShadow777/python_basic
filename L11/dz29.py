f96caf2638c7d4a07868dde4208fe470c2162225
NightShadow777 <rudolf_miller@ukr.net>
2022-12-26 23:20:45 +0200

    add dz29

--- L11/dz29.py
+++ L11/dz29.py
@@ -1,4 +1,6 @@
-def generate_cube_numbers(int_param):
+def generate_cube_numbers(int_param: int) -> int:
+    '''функция-генератор generate_cube_numbers, которая формирует набор кубов чисел,
+    начиная с числа 2 и до указанной Вами величины. '''
     for i in range(2, int_param):
         res = pow(i, 3)
         if res < int_param:
@@ -8,4 +10,6 @@ def generate_cube_numbers(int_param):
 
 print(list(generate_cube_numbers(10)))
 print(list(generate_cube_numbers(100)))
+
 print(list(generate_cube_numbers(1000)))
+print(list(generate_cube_numbers(2000)))
