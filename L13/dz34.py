3a4d9fe062e7e38b318864773b2f5d9b1b3fda8d
NightShadow777 <rudolf_miller@ukr.net>
2023-01-05 02:55:55 +0200

    дописал dz34

--- L13/dz34.py
+++ L13/dz34.py
@@ -1,20 +1,3 @@
-'''
-Создать класс цифрового счетчика. В классе реализовать методы:
-
-    установка максимального значения счетчика,
-    установка минимального значения счетчика
-    установка начального значения счётчика
-    метод увеличения счетчика на 1. Метод можно вызывать до тех пор, пока значение не достигнет максимума.
-    Потом - выкинуть (raise) исключение ValueError, с описанием, что достигнут максимум
-    уменьшения счетчика на 1. Метод можно вызывать до тех пор, пока значение не достигнет минимума. Потом -
-    выкинуть (raise) исключение ValueError, с описанием, что достигнут минимум
-    возвращения текущего значения счётчика
-
-Начальное, минимальное и максимальное значения счетчика, также могут быть добавлены в метод инициализации экземпляра класса.
-
-
-Приблизительный каркас для класса и варианты проверки. Вам нужно дописать необходимое вместо pass
-'''
 class Counter():
     def __init__(self, current=1, min_value=0, max_value=10):
         self.current = current
@@ -24,18 +7,21 @@ class Counter():
     def set_current(self, start):
         self.current = start
 
-
     def set_max(self, max_max):
-        pass
+        return max_max + 1
 
     def set_min(self, min_min):
-        pass
+        return min_min - 1
 
     def step_up(self):
-        pass
+        self.current = self.set_max(self.current)
+        if self.current > self.max_value:
+            raise ValueError("Достигнут максимум")
 
     def step_down(self):
-        pass
+        self.current = self.set_min(self.current)
+        if self.min_value < self.current:
+            raise ValueError("Достигнут минимум")
 
     def get_current(self):
         return self.current
@@ -46,8 +32,13 @@ counter.step_up()
 counter.step_up()
 counter.step_up()
 print(counter.get_current())  # 10
+
 try:
     counter.step_up()  # ValueError
 except ValueError as e:
     print(e) # Достигнут максимум
+    try:
+        counter.step_down()
+    except ValueError as e:
+        print(e)
 print(counter.get_current())  # 10
